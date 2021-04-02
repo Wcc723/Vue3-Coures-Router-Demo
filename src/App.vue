c20a1343d34d4b623a5e0fe39527deb395597a3c
wcc723 <agito723@gmail.com>
2021-04-02 13:15:34 +0800

    加入 Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,33 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link :to="{
-        name: '新增頁面'
-      }">新增頁面</router-link>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button class="navbar-toggler" type="button"
+        data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/about" class="nav-link">About</router-link>
+          </li>
+          <li class="nav-item" >
+            <router-link class="nav-link"
+              :to="{
+                name: '新增頁面',
+              }">新增頁面</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <router-view/>
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
